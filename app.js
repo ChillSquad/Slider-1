90577e879cbb68f8788e831d96be1350a5308438
Andrey Tyukachev <tyukachev2014@mail.ru>
2024-05-20 21:02:26 +0300

    added keydown

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const bar = document.getElementById("bar");
 const wrapper = document.getElementById("wrap");
 let activeSlideIndex = 0;
 
+document.addEventListener("keydown", (event) => {
+  console.log(event.key);
+  if (event.key === "ArrowUp") {
+    clickEvent("up");
+  } else if (event.key === "ArrowDown") {
+    clickEvent("down");
+  }
+});
+
 bar.style.top = `-${(slideCount - 1) * 100}vh`;
 
 upBtn.addEventListener("click", () => {
@@ -18,14 +27,14 @@ downBtn.addEventListener("click", () => {
 
 function clickEvent(direction) {
   if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex === slideCount) {
-      activeSlideIndex = 0;
-    }
-  } else if (direction === "down") {
     activeSlideIndex--;
     if (activeSlideIndex < 0) {
       activeSlideIndex = slideCount - 1;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0;
     }
   }
 
